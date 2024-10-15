3dae0ac22fcbbc1ebb628b7518bc454af3ea8a41
Frank Eduardo <109195231+franklg1911@users.noreply.github.com>
2024-10-15 14:30:01 -0500

    Delete basics directory

--- basics/Strings/ImpresionVariables/Ejercicio.py
+++
@@ -1,9 +0,0 @@
-#Escribe un programa que solicite al usuario ingresar su nombre, edad, y altura. Luego, imprime un mensaje que combine estos datos en una oración.
-
-print("\nImpresion de mensaje\n")
-
-nombre = input("Por favor ingrese su nombre: \n")
-edad = int(input("Por favor ingresa tu edad: \n"))
-altura = float(input("Por favor ingresa tu altura: \n"))
-
-print(f"Tu nombre es {nombre} tienes una edad de {edad} y una altura de {altura}")
