2fb96e219e38e6a13af51ded67c4aaf2d8f8f544
Frank Eduardo <109195231+franklg1911@users.noreply.github.com>
2024-10-15 12:58:05 -0500

    Add files via upload

---
+++ basics/Strings/ConversionNumeros/Ejercicio.py
@@ -0,0 +1,11 @@
+# Pide al usuario que ingrese un número flotante, luego convierte este número a entero y muestra ambos valores en la consola.
+
+print("\nConversión de flotante a entero\n")
+
+numero_flotante = float(input("Ingresa un número flotante:\n"))
+
+numero_entero = int(numero_flotante)
+
+print("El númeor flotante es:" , numero_flotante)
+
+print("El número convertido de flotante a entero es : " , numero_entero)
